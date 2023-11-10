a8b89dde16df56e58e760afb25d2d509d7124936
Glauber8289 <Petersonglauber@hotmail.com>
2023-11-10 14:34:50 -0300

    Automatizacao Finalizada Idash

--- main.py
+++ main.py
@@ -13,78 +13,114 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from datetime import datetime, timedelta
 
 options = Options()
 options.headless = False #Excutar de forma visivel
 
+##Acessando o navegador
 navegador = webdriver.Chrome(options=options)
 
+##Link para acessar o site
 link= "https://idash.ifcshop.net/auth/realms/chat-bot-b2b/protocol/openid-connect/auth?client_id=chat-bot-b2b-app&redirect_uri=https%3A%2F%2Fidash.ifcshop.net%2F&state=7d3ac4d3-a588-4efe-acbd-8689666c8530&response_mode=fragment&response_type=code&scope=openid&nonce=ef238d2a-f304-4122-84b9-137299ed11ed"
-
 navegador.get(url=link)
 time.sleep(3)
 
-
+##Inserindo usuario
 inputUsuario = navegador.find_element(by=By.ID,value="username")
-
 inputUsuario.send_keys("sophia.coelho")
 time.sleep(5)
 
+##Inserindo senha
 inputSenha = navegador.find_element(by=By.ID,value="password")
 inputSenha.send_keys("S0PH14@pires")
 time.sleep(5)
 
-
-
-
+##Clicando no botão Sign In
 buttonLogin = navegador.find_element(by=By.ID,value="kc-login")
 buttonLogin.click()
 time.sleep(5)
 
-#######################################################################
-
-#Erro corrigir
-##linkRelatorios = navegador.find_element(by=By.TAG_NAME, value= "SPAN")
-##linkRelatorios.click
-##time.sleep(3)
-
-################################################################################
-
-
-linkR="https://idash.ifcshop.net/reports/attendance-quality"
-
-navegador.get(url=linkR)
+##Acessando a pagina quality
+linkQ="https://idash.ifcshop.net/reports/attendance-quality"
+navegador.get(url=linkQ)
 time.sleep(5)
 
-
-
-
-
-
-
-
+##Clicando no botão Selecionar todos
 clickable = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(1) > div > div > button')
 ActionChains(navegador)\
 .click(clickable)\
 .perform()
 time.sleep(10)
 
+
+##Variavel para pegar a data em D-1
+data_ant = datetime.now() - timedelta(days=1)
+data_ant_form = data_ant.strftime("%d/%m/%Y")
+
+##Preenchendo data anterior quality
 text_input = navegador.find_element(By.CSS_SELECTOR, "#\:r2\:")
 ActionChains(navegador)\
-.send_keys_to_element(text_input, "08/11/2023")\
+.send_keys_to_element(text_input, data_ant_form)\
 .perform()
 time.sleep(10)
 
-clickable1 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-1a9w728 > button')
+##Clicando no botão para gerar o relatório
+GerarRelatorio = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-1a9w728 > button')
+ActionChains(navegador)\
+.click(GerarRelatorio)\
+.perform()
+time.sleep(10)
+
+
+##Acessando a pagina Retenção    
+linkR="https://idash.ifcshop.net/reports/subject"
+navegador.get(url=linkR)
+time.sleep(5)
+
+##Clicando no botão Selecionar todos
+clickable1 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(1) > div > div > button')
 ActionChains(navegador)\
 .click(clickable1)\
 .perform()
 time.sleep(10)
 
+##Preenchendo data anterior Retenção
+text_input1 = navegador.find_element(By.CSS_SELECTOR, "#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(3) > div > div")
+ActionChains(navegador)\
+.send_keys_to_element(text_input1, data_ant_form)\
+.perform()
+time.sleep(10)
 
+##Clicando no botão para gerar o relatório
+GerarRelatorio1 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-1a9w728 > button')
+ActionChains(navegador)\
+.click(GerarRelatorio1)\
+.perform()
+time.sleep(15)
 
+##Acessando a pagina Tickets    
+linkT="https://idash.ifcshop.net/reports/tickets"
+navegador.get(url=linkT)
+time.sleep(5)
 
+##Clicando no botão Selecionar todos
+clickable2 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(1) > div > div > button')
+ActionChains(navegador)\
+.click(clickable2)\
+.perform()
+time.sleep(10)
 
+##Preenchendo data anterior Tickets
+text_input2 = navegador.find_element(By.CSS_SELECTOR, "#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(3) > div > div")
+ActionChains(navegador)\
+.send_keys_to_element(text_input2, data_ant_form)\
+.perform()
+time.sleep(10)
 
-
+##Clicando no botão para gerar o relatório
+GerarRelatorio2 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-1a9w728 > button')
+ActionChains(navegador)\
+.click(GerarRelatorio2)\
+.perform()
+time.sleep(15)
