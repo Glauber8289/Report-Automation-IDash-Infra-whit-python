c1ba76180ffc624f1ba1d393b60a516c4680d911
Glauber8289 <Petersonglauber@hotmail.com>
2023-11-09 23:57:03 -0300

    Automatizacao Finalizada

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge import options
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.chrome import options
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import Chrome
 import time
 from bs4 import BeautifulSoup as bs
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.mouse_button import MouseButton
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 options = Options()
 options.headless = False #Excutar de forma visivel
 
-navegador = webdriver.Edge(options=options)
+navegador = webdriver.Chrome(options=options)
 
 link= "https://idash.ifcshop.net/auth/realms/chat-bot-b2b/protocol/openid-connect/auth?client_id=chat-bot-b2b-app&redirect_uri=https%3A%2F%2Fidash.ifcshop.net%2F&state=7d3ac4d3-a588-4efe-acbd-8689666c8530&response_mode=fragment&response_type=code&scope=openid&nonce=ef238d2a-f304-4122-84b9-137299ed11ed"
 
@@ -18,41 +27,30 @@ time.sleep(3)
 
 
 inputUsuario = navegador.find_element(by=By.ID,value="username")
-<<<<<<< HEAD
+
 inputUsuario.send_keys("sophia.coelho")
-time.sleep(3)
+time.sleep(5)
 
 inputSenha = navegador.find_element(by=By.ID,value="password")
 inputSenha.send_keys("S0PH14@pires")
-time.sleep(3)
-=======
-inputUsuario.send_keys("Usuario")
-time.sleep(2)
+time.sleep(5)
 
-inputSenha = navegador.find_element(by=By.ID,value="password")
-inputSenha.send_keys("Senha")
-time.sleep(2)
->>>>>>> 4bb47079b5aba009cfdb2fe5b1a0d6f693fd2c5f
+
+
 
 buttonLogin = navegador.find_element(by=By.ID,value="kc-login")
 buttonLogin.click()
-time.sleep(3)
-<<<<<<< HEAD
+time.sleep(5)
 
+#######################################################################
 
-## Erro corrigir
+#Erro corrigir
 ##linkRelatorios = navegador.find_element(by=By.TAG_NAME, value= "SPAN")
 ##linkRelatorios.click
 ##time.sleep(3)
 
+################################################################################
 
-##linkRelatorios1 = navegador.find_element(by=By.TAG_NAME, value= "svg")
-##linkRelatorios1.click
-##time.sleep(2)
-
-##linkQualidade = navegador.find_element(by=By.TAG_NAME,value="LI")
-##linkQualidade.click
-##time.sleep(2)
 
 linkR="https://idash.ifcshop.net/reports/attendance-quality"
 
@@ -60,22 +58,33 @@ navegador.get(url=linkR)
 time.sleep(5)
 
 
-SelectAll = navegador.find_element (by=By.CSS_SELECTOR,value='#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(1) > div > div > button')
-##SelectAll.click
-print(SelectAll)
-time.sleep(5)
 
 
-##SelectAll = navegador.find_element (by=By.ID,value='root')
-##SelectAll.click
-##time.sleep(5)
 
-###GeneratetAll = navegador.find_element (by=By.LINK_TEXT,value='')
-##GeneratetAll.click
-##time.sleep(5)
 
-##inputD1 = navegador.find_element(by=By.ID,value=":rf:")
-##inputD1.send_keys("07/11/2023")
-##time.sleep(10)
-=======
->>>>>>> 4bb47079b5aba009cfdb2fe5b1a0d6f693fd2c5f
+
+
+clickable = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(1) > div > div > button')
+ActionChains(navegador)\
+.click(clickable)\
+.perform()
+time.sleep(10)
+
+text_input = navegador.find_element(By.CSS_SELECTOR, "#\:r2\:")
+ActionChains(navegador)\
+.send_keys_to_element(text_input, "08/11/2023")\
+.perform()
+time.sleep(10)
+
+clickable1 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-1a9w728 > button')
+ActionChains(navegador)\
+.click(clickable1)\
+.perform()
+time.sleep(10)
+
+
+
+
+
+
+
